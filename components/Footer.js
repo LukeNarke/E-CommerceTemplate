008faedfc1e6748143a39f5b0352ad07bcc288c4
Luke Narke <LNarke@gmail.com>
2021-08-15 19:41:32 +0700

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => (
+  <footer className="container xl:max-w-screen-xl mx-auto p-6 mt-8 text-center">
+    <p>
+      Any questions? Contact me at{" "}
+      <a
+        href="mailto:lnarke@gmail.com?subject=Question%20about%20E-Commerce Website"
+        className="underline text-rose-500"
+      >
+        LNarke@gmail.com
+      </a>
+    </p>
+  </footer>
+);
+
+export default Footer;
