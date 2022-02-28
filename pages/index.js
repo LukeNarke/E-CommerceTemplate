f5a15d38474c8c8d0a35072bbdbdc478968ad3d8
Luke Narke <74273366+LukeNarke@users.noreply.github.com>
2022-02-28 07:51:12 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import products from "products";
 export default function Home() {
   const [disabled, setDisabled] = useState(false);
 
+//   if CSS gets bigger, use a file instead of Tailwind in the classnames
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
